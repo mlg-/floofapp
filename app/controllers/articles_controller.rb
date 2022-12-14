8b5a08ded30679128a922795235aa5e97ed408ed
Melissa Leigh Gore <melissa@actbluetech.com>
2022-12-14 13:17:36 -0500

    Little bit of boilerplate for session 3!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+  def new
+    @article = Article.new
+    @users = User.all.map { |u| [ u.email, u.id ] }
+  end
   end
 end
